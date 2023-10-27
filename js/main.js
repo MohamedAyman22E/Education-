666ec7f7ae425c1e37c2d7a465ecb47d2785e362
moAymanEz <moAyman@gmail.com>
2023-10-27 22:40:14 +0200

    27-10-2023 10:40 pm

--- js/main.js
+++ js/main.js
@@ -9,3 +9,24 @@ menu.addEventListener("click", () => {
     menuI.classList.replace("fa-xmark", "fa-bars");
   }
 });
+
+let section = document.querySelectorAll("section");
+let navList = document.querySelectorAll("header nav .navList li a");
+window.onscroll = () => {
+  section.forEach(sec => {
+    let top = window.scrollY,
+      offset = sec.offsetTop - 10,
+      height = sec.offsetHeight,
+      id = sec.getAttribute("id");
+
+    if (top >= offset && top < offset + height) {
+      document
+        .querySelector("header nav .navList li a[href*=" + id + "]")
+        .classList.add("active");
+    } else {
+      document
+        .querySelector("header nav .navList li a[href*=" + id + "]")
+        .classList.remove("active");
+    }
+  });
+};
